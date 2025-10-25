6b139011b8f359ce03adbba671f6bdc1d9e3a749
Joseph Karl Labuguen <labuguenkarl35@gmail.com>
2025-10-25 22:01:12 +0800

    update server and dashb

--- server.js
+++ server.js
@@ -14,11 +14,16 @@ db.connect();
 
 const userId = 1;
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 
 app.get("/dashboard", async (req, res) => {
     try {
         const budget = await db.query("SELECT * FROM budget WHERE userId = $1;", [userId])
-     res.render("dashb.ejs", {budget: budget.rows[0]});
+        const categories = await db.query("SELECT * FROM category WHERE userId = $1;", [userId])
+     res.render("dashb.ejs", {budget: budget.rows[0], categories: categories.rows});
     } catch (error) {
         console.log(error);
     }
